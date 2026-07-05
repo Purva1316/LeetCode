b0bcf805739ac120f345d47e52e6c35c97cc59a4
Purva <purvampatil07@gmail.com>
2026-07-05 13:16:12 +0530

    Time: 0 ms (100%), Space: 43.1 MB (57.24%) - LeetHub

---
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int largestAltitude(int[] gain) {
+        int curAlt=0;
+        int maxAlt=0;
+        for(int i=0;i<gain.length;i++){
+            curAlt += gain[i];
+            maxAlt=Math.max(maxAlt,curAlt);
+        }
+        return maxAlt;
+    }
+}
